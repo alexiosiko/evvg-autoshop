27b1c8450b093aeaf8751eeb70a1815328efe6e8
alexiosiko <jemimassyrup@gmail.com>
2023-09-25 14:37:58 -0700

    Update CreateAppointment.tsx

--- components/appointments/CreateAppointment.tsx
+++ components/appointments/CreateAppointment.tsx
@@ -43,19 +43,16 @@ export default function CreateAppointment() {
 	const detailsRef = useRef<HTMLInputElement>(null);
 	const vinRef = useRef<HTMLInputElement>(null);
 
-	function handleOnSetDate(newDate: Date| undefined): void {
-		if (!newDate)
+	function handleOnSetDate(day: Date | undefined): void {
+		if (!day)
 			return;
-		let mergedDate;
-		try {
-			if (date == null)
-				mergedDate = new Date();
-			else
-				mergedDate = new Date(date);
-			mergedDate.setDate(newDate.getDate());
-			setDate(mergedDate);
-		} catch (e) {}
+		let mergedDate = new Date(day);
 
+		if (date != null) {
+			mergedDate.setHours(date.getHours());
+			mergedDate.setMinutes(date.getMinutes());
+		}
+		setDate(mergedDate);
 	}
 
 	function handleSetTime(hours: number, minutes: number) {
