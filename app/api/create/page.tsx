97dd23652720209b14a6f402a1ab73fad15c9028
Alexi <jemimassyrup@gmail.com>
2023-09-24 22:22:56 -0700

    Back button

--- app/api/create/page.tsx
+++ app/api/create/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Create() {
 	return (
 		<>
-			<Link href="/admin/georgeaccount">
+			<Link href="/api/manage">
 				<Button>Back</Button>
 			</Link>
 			<CreateAppointment />
